7095463dee6a905f71c7fd2b1c96e277ca4b8e96
Shaman <infografika@inbox.ru>
2022-07-13 20:13:37 +0700

    simple change

--- app.py
+++ app.py
@@ -4,8 +4,6 @@ from aiogram import Bot, Dispatcher, executor, types
 from config import MY_API_KEY
 
 
-
-
 # подключение бота
 logging.basicConfig(level=logging.INFO)
 bot = Bot(token=MY_API_KEY)
@@ -21,19 +19,15 @@ async def send_welcome(message: types.Message):
     await message.reply('Какую операцию вы хотите совершить', reply_markup=keyboard)
      
 
-
-
 @dp.message_handler(lambda message: message.text == 'Доход')
 async def echo(message: types.Message):
    
    await message.reply("Отличный выбор!", reply_markup=types.ReplyKeyboardRemove())
 
 
-
 @dp.message_handler(lambda message: message.text == 'Расход')
 async def echo(message: types.Message):
    await message.reply("Не завидую", reply_markup=types.ReplyKeyboardRemove())
-
 
 
 if __name__ == '__main__':
